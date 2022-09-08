181f6e34a22168582ce50e21e82ae18424de4953
JuliaHoleiko <juliagoleiko2341@gmail.com>
2022-09-08 06:14:40 +0300

    added delete in avl tree

---
+++ src/com/company/Node.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Node {
+    int key, height;
+    Node left, right;
+
+    public Node(int key) {
+        this.key = key;
+        this.height = 1;
+    }
+}
+
