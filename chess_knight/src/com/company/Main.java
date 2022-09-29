32c83d05462951283f2ee4176c1e6192200f6682
JuliaHoleiko <juliagoleiko2341@gmail.com>
2022-09-29 21:14:38 +0300

    modify

--- chess_knight/src/com/company/Main.java
+++ chess_knight/src/com/company/Main.java
@@ -1,93 +1,42 @@
 package com.company;
 
-import java.io.*;
-import java.util.ArrayList;
-
-
 public class Main {
-    static int[][] doMatrixForBoard(int n){
-        // int n = 8;
+    static int[][] doMatrixForBoard(int n) {
+
         int[][] matrix = new int[n * n][n * n];
-
         int[] row = {2, 2, -2, -2, 1, 1, -1, -1};
-
-        int col[] = {-1, 1, 1, -1, 2, -2, 2, -2};
+        int[] col = {-1, 1, 1, -1, 2, -2, 2, -2};
         int x, y, pos;
-
         for (int i = 0; i < n * n; i++) {
-
-
             for (int j = 0; j < 8; j++) {
                 x = i / 8;
                 y = i % 8;
-
                 x = x + row[j];
                 y = y + col[j];
-
                 if ((x >= 0) && (y >= 0) && (x <= 7) && (y <= 7)) {
                     pos = x * 8 + y;
-
                     matrix[i][pos] = 1;
                 }
             }
-
         }
-
         return matrix;
     }
-    static int[]  readFile() throws Exception {
-        File file = new File("task.txt");
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        int input[] = new int[5];
 
-        int i = 0;
-        while ((st = br.readLine()) != null){
-            if(st.contains(",")){
-                String arr[] = st.split(",");
-                int pos_x,pos_y;
-                pos_x = Integer.parseInt(arr[0]);
-                pos_y = Integer.parseInt(arr[1]);
-                input[i] = pos_x;
-                i++;
-                input[i] = pos_y;
-                i++;
-
-                System.out.println(pos_x+ " "+ pos_y);
-                //System.out.println(pos_y);
-            }
-            else {
-                System.out.println(st);
-                input[i] = Integer.parseInt(st);
-                i++;
-            }
-        }
-        System.out.println();
-        for(int j = 0; j <5;j++){
-            System.out.println(input[j]);
-        }
-        return input;
-    }
 
     public static void main(String[] args) throws Exception {
-        int arr[] = readFile();
-
-        int matrix[][] = doMatrixForBoard(arr[0]* arr[0]);
-        Path p = new Path(arr[0]*arr[0], 0,62);
-       int k = p.dijkstra(matrix, 0);
-        System.out.println();
-        System.out.println(k);
         WorkWithFile w = new WorkWithFile();
-        w.addFile(k);
-
+        int[] arr = w.readFile();
+        int size = arr[0] * arr[0];
+        int start = arr[1] * 8 + arr[2] ;
+        int end = arr[3] * 8 + arr[4] ;
+        int[][] matrix = doMatrixForBoard(arr[0] * arr[0]);
+        Path path = new Path(size, start, end, matrix);
+        int shortest_distance = path.shortestDistance();
+        System.out.println(shortest_distance);
+        w.addFile(shortest_distance);
 
 
     }
 
 
-
-
-
-
-
-    }
+}
--- chess_knight/src/com/company/Path.java
+++ chess_knight/src/com/company/Path.java
@@ -1,79 +1,67 @@
 package com.company;
 
 public class Path {
-    int graph_length, start, end;
+    int graph_length;
+    int start;
+    int end;
+    int[][] graph;
 
-    public Path(int graph_length, int start, int end) {
+    public Path(int graph_length, int start, int end, int[][] graph) {
         this.graph_length = graph_length;
         this.start = start;
         this.end = end;
+        this.graph = graph;
     }
 
-    int minDistance(int distance[], Boolean visited[]){
+    int minDistance(int[] distance, Boolean[] visited) {
         int min = Integer.MAX_VALUE;
         int near_vertex = -1;
-
-        for (int i = 0; i < graph_length; i++){
-            if ( !visited[i] && (distance[i] <= min)){
+        for (int i = 0; i < graph_length; i++) {
+            if (!visited[i] && (distance[i] <= min)) {
                 min = distance[i];
                 near_vertex = i;
             }
-
         }
         return near_vertex;
     }
 
-    int dijkstra (int graph [][], int start){
-        int distance[] = new int[graph_length];
-        int parents[] = new int[graph_length];
+    int shortestDistance() {
+        int[] distance = new int[graph_length];
+        int[] parents = new int[graph_length];
         parents[start] = -1;
+        Boolean[] visited = new Boolean[graph_length];
 
-        Boolean visited [] = new Boolean[graph_length];
-        for (int i = 0; i < graph_length; i++){
+        for (int i = 0; i < graph_length; i++) {
             distance[i] = Integer.MAX_VALUE;
             visited[i] = false;
         }
         distance[start] = 0;
 
-
-        for (int i = 0; i < graph_length; i++){
+        for (int i = 0; i < graph_length; i++) {
             int visited_vertex = minDistance(distance, visited);
             visited[visited_vertex] = true;
-            for (int v = 0; v < graph_length; v++){
-                if (!visited[v] && graph[visited_vertex][v] !=0 && distance[visited_vertex] != Integer.MAX_VALUE
-                        && distance[visited_vertex] + graph[visited_vertex][v] < distance[v])  {
+            for (int v = 0; v < graph_length; v++) {
+                if (!visited[v] && graph[visited_vertex][v] != 0 && distance[visited_vertex] != Integer.MAX_VALUE
+                        && distance[visited_vertex] + graph[visited_vertex][v] < distance[v]) {
                     distance[v] = distance[visited_vertex] + graph[visited_vertex][v];
                     parents[v] = visited_vertex;
-
                 }
-
             }
-
         }
-        printResults(distance,parents);
+        printResults(distance, parents);
         return distance[end];
     }
 
-    void printResults(int dist[], int[] parents) {
-        System.out.println(
-                "Vertex \t\t Distance from Source");
-
-            System.out.println(start +"to "+end+ ": "+  " \t\t distance:" + dist[end] + "   path: " );
-
+    void printResults(int[] dist, int[] parents) {
         System.out.println();
+        System.out.println("From " + start + " to " + end + ": " + " distance:" + dist[end] + "   path: ");
         printPath(end, parents);
-
     }
-    void printPath(int currentVertex,
-                   int[] parents)
-    {
-        if (currentVertex == -1)
-        {
+    void printPath(int currentVertex, int[] parents) {
+        if (currentVertex == -1) {
             return;
         }
         printPath(parents[currentVertex], parents);
         System.out.print(currentVertex + " ");
     }
-
-
 }
